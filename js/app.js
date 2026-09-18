d555c170055d2204887dffb89d8cfaeb3c2abd04
oinventor <oinventor.official@gmail.com>
2026-09-18 12:51:44 -0300

    Eventos estao sendo passados para o script

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-const navLinks = document.querySelector('header').querySelectorAll('a')
+// Get staf from doc
+const navLinks = document.querySelector('header').querySelectorAll('a');
+
+// Important vars
+let events = [];
+let ids = 0;
+
+// Nav
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         navLinks.forEach(navLink => {
@@ -6,4 +13,19 @@ navLinks.forEach(link => {
         });
         document.getElementById(link.dataset.view).classList.remove('d-none');
     });
+});
+
+// Events
+document.getElementById('btnNewEvent').addEventListener('click', () => {
+    ids++
+    const event = {
+        id: ids,
+        tittle: document.getElementById('eventName').value,
+        type: document.getElementById('eventType').value,
+        local: document.getElementById('eventLocal').value,
+        data: document.getElementById('eventDate').value,
+        description: document.getElementById('eventDescription').value,
+        status: 1,
+    }
+    console.log(event);
 });
